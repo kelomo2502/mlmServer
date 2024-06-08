baa2360a178a2cdb9ba354913f9128720de9fca8
unknown <kelvinoye@gmail.com>
2024-06-08 19:09:48 +0100

    Fixed undefined token for getLoginStatus

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3100", "http://localhost:5173"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
